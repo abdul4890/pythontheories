eeb24ea6244214b876ccaf399f99c41a768e5e49
abdul4890 <abdulraheman4890@gmail.com>
2023-03-08 20:18:19 +0530

    ha

--- newproject.py
+++ newproject.py
@@ -4,3 +4,8 @@ print("raheman")
 print("raheman")
 a = 24;
 print(type(a))
+# adding 2 numbers
+num1 = 44
+num2 = 7
+sum = num1 + num2
+print("the sum of {0} and {1} is {2}".format(num1, num2, sum))  

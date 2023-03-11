8fc9df25e1208d436c584bf81f0a473c2b7f3152
abdul4890 <abdulraheman4890@gmail.com>
2023-03-11 20:14:23 +0530

    Guess the Number game code

---
+++ Exercise1.py
@@ -0,0 +1,21 @@
+# Guess number game
+# Computer will detect if the number is too low or high than the actual number
+# Store the actual number in a variable
+#4 you have to finally tell how many attempts did you take to guess the number
+
+# Declaring variables
+actual_number = 54
+attempts = 0
+
+while True:
+    attempts += 1
+    guess = int(input("guess the number: /n"))
+    if guess<actual_number:
+     print("your guess was too low")
+     elif guess>actual_number:
+     print("your guess was too high")
+
+else:
+    print(you guessed the number in {attempts} attempts)
+    break
+print(thanks for playing)

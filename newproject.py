14a236b5b98e646730ca531698b862daaea5da20
abdul4890 <abdulraheman4890@gmail.com>
2023-04-03 15:39:32 +0530

    new

--- newproject.py
+++ newproject.py
@@ -18,4 +18,5 @@ sum = num1 + num2
 print("the sum of {0} and {1} is {2}".format(num1, num2, sum)) 
 print("raheman")    
 print("raheman")
-input('enter the number :')
+input('enter the number    :')
+

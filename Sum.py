b5a955cc75e69f33ed8668c4d81d76157ef38e74
abdul4890 <abdulraheman4890@gmail.com>
2023-04-06 01:33:00 +0530

    new

--- Sum.py
+++ Sum.py
@@ -3,10 +3,11 @@
 
 a = 234
 b = 564
-
 print(f'the sum of {a} and {b} is {a+b}')
 
 number = input("insert the number: ")
-
 if number == 20:
-    print(f"sahi hai")
+ print("sahi hai")
+else:
+    print("galat hai")
+    

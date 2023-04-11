c0fc58f45f9ecdf2e476a20570de2ab488079a56
abdul4890 <abdulraheman4890@gmail.com>
2023-04-11 09:23:00 +0530

    Based Progamme

--- Sum.py
+++ Sum.py
@@ -6,12 +6,12 @@ b = 564
 print(f'the sum of {a} and {b} is {a+b}')
 
 number = 12 
-guess = input("write teh number: ")
+guess = int(input("write teh number: "))
 
-x = range(6)
-for n in x:
-  print(n)
-
+if number>guess:
+    print("guess number chota hai")
+elif number<guess:
+    print("guess number bada hai")
 
 
 

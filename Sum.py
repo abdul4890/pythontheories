39c8dc1d1330a857cb1bb3b14d830ae82b9d4048
abdul4890 <abdulraheman4890@gmail.com>
2023-04-14 01:26:00 +0530

    new

--- Sum.py
+++ Sum.py
@@ -1,17 +1,4 @@
-
-  
-
-a = 234
-b = 564
-print(f'the sum of {a} and {b} is {a+b}')
-
-number = 12 
-guess = int(input("write teh number: "))
-
-if number>guess:
-    print("guess number chota hai")
-elif number<guess:
-    print("guess number bada hai")
+name = razi
 
 
 

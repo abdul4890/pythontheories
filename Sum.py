022d2381efe27fd355673e4d013fc77a7eff2d34
abdul4890 <abdulraheman4890@gmail.com>
2023-04-15 18:46:57 +0530

    Dice Simulator

--- Sum.py
+++ Sum.py
@@ -1,6 +1,9 @@
-
-i = 1
-while(i <= 5):
-    print("Good Morning")
-    i = i + 1
-
+import random
+while True:
+ print(''' 1: roll the dice 2. To exit ''')
+ user = int(input('enter the number:'))
+ if user == 1:
+    number = random.randint(1,6)
+    print(number)
+ else:
+  break

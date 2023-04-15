c11793b68d62559ebc366b02be7c07df4c6a54be
abdul4890 <abdulraheman4890@gmail.com>
2023-04-15 18:48:22 +0530

    new dice

--- Sum.py
+++ Sum.py
@@ -1,9 +1,9 @@
 import random
-while True:
- print(''' 1: roll the dice 2. To exit ''')
- user = int(input('enter the number:'))
- if user == 1:
+
+print(''' 1: roll the dice 2. To exit ''')
+user = int(input('enter the number:'))
+if user == 1:
     number = random.randint(1,6)
     print(number)
- else:
-  break
+else:
+ print('exit')

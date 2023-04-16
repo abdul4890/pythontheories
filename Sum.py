ecb145881ce929940cd4114fa5bbb16dce462777
abdul4890 <abdulraheman4890@gmail.com>
2023-04-17 02:15:52 +0530

    new

--- Sum.py
+++ Sum.py
@@ -1,9 +1,4 @@
-import random
+# programe to get unique vowels from list
 
-print(''' 1: roll the dice 2. To exit ''')
-user = int(input('enter the number:'))
-if user == 1:
-    number = random.randint(1,6)
-    print(number)
-else:
- print('exit')
+l = "my name is razi ansaru"
+l.len()

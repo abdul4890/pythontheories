af6fe3b08a3f0e51bbc936aec5509685b17a491d
abdul4890 <abdulraheman4890@gmail.com>
2023-04-16 16:02:42 +0530

    Prime Number Programme using FOR loop

--- newproject.py
+++ newproject.py
@@ -1,5 +1,9 @@
 number = int(input("enter the number: "))
 for i in range(2, number):
  if (number % i) == 0:
-  print("not prime")
+   print("not prime")
+   break
+ else:
+  print("prime")
   break
+   

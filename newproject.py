08acd4071976adbfa5a9d5ab0487c4c09320f645
abdul4890 <abdulraheman4890@gmail.com>
2023-04-16 15:43:16 +0530

    prime

--- newproject.py
+++ newproject.py
@@ -1,30 +1,5 @@
-print("hello world!")
-print("abdul")
-print("raheman")    
-print("raheman")
-a = 24;
-print(type(a))
-# adding 2 numbers
-num1 = 44
-num2 = 7
-sum = num1 + num2
-print("the sum of {0} and {1} is {2}".format(num1, num2, sum)) 
-b = 1;
-print(b)
-3 == ("3")
-num1 = 44
-num2 = 7
-sum = num1 + num2
-print("the sum of {0} and {1} is {2}".format(num1, num2, sum)) 
-print("raheman")    
-print("raheman")
-
-name = "razi"
-for char in name:
-    print(char, end = "")
-    i += 10
-    while i <= 10:
-        print(i)
-        i+= 1
-
-
+number = int(input("enter the number: "))
+for i in range(2, number):
+ if (number % i) == 0:
+  print("not prime")
+  break

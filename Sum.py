b88e8fed735f743a6c8a73c88f698d34a0ff0348
abdul4890 <abdulraheman4890@gmail.com>
2023-04-18 04:12:10 +0530

    Reversing the String Characters

--- Sum.py
+++ Sum.py
@@ -1,3 +1,8 @@
 
-a = ("i am going home or i am going hostelvplus going again")
+a = ("i am going home or i am going hostel plus going")
 a.count("going")
+
+s = ("i am going home or i am going hostel plus going")
+reversed=''.join(reversed(s)) 
+# .join() method merges all of the characters resulting from the reversed iteration into a new string
+print(reversed) 

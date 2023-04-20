2e600d22d0048feb4acb1fe306fe8304ae99abbb
abdul4890 <abdulraheman4890@gmail.com>
2023-04-20 20:26:16 +0530

    new

--- Sum.py
+++ Sum.py
@@ -6,3 +6,8 @@ s = ("i am going home or i am going hostel plus going")
 reversed=''.join(reversed(s)) 
 # .join() method merges all of the characters resulting from the reversed iteration into a new string
 print(reversed)
+
+emp = {'names: Razi', 'experience: 4 years', 'age : 30',
+       'names: Abdul', 'experience: 5 years', 'age : 32'
+       
+       } 

9dfd5d33dfb7e254e94c7584265809361bac805d
abdul4890 <abdulraheman4890@gmail.com>
2023-04-24 16:30:17 +0530

    new

--- Sum.py
+++ Sum.py
@@ -2,8 +2,4 @@
 a = ("i am going home or i am going hostel plus going")
 a.count("going")
 
-s = ("i am going home or i am going hostel plus going")
-reversed=''.join(reversed(s)) 
-# .join() method merges all of the characters resulting from the reversed iteration into a new string
-print(reversed)
 

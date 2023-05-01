a954d56cd4cb16e5560c8acef5c95a172355a948
abdul4890 <abdulraheman4890@gmail.com>
2023-05-01 16:12:06 +0530

    new

--- Practice.py
+++ Practice.py
@@ -1,3 +1,8 @@
-def prime():
-    
+class Test: ##Defining the class
+    def m1 (self, a, b): ##defining a method
+        self.a = a
+        self.b = b
+        print(a + b)
+t = Test() ##defining the object
+t.m1(23, 45) ##method calling
 

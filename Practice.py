330a103cb885164553c3a45fdeff77a0e1d83656
abdul4890 <abdulraheman4890@gmail.com>
2023-05-03 01:44:27 +0530

    n

--- Practice.py
+++ Practice.py
@@ -2,7 +2,7 @@ class Test: ##Defining the class
     def m1 (self, a, b): ##defining a method
         self.a = a
         self.b = b
-        print(a + b)
+        print(a + b)     
 t = Test() ##defining the object
 t.m1(23, 45) ##method calling
 

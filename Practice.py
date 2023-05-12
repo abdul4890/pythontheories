17ceb18965107dab7842f712daaa6151058d4fa5
abdul4890 <abdulraheman4890@gmail.com>
2023-05-12 23:02:18 +0530

    new

--- Practice.py
+++ Practice.py
@@ -1,13 +1,9 @@
-# class Test: ##Defining the class
-#     def m1 (self, a, b): ##defining a method
-#         self.a = a
-#         self.b = b
-#         print(a + b)     
-# t = Test() ##defining the object
-# t.m1(2353,5850) ##method calling
-
-## Pythong script to keep the kursor moving 
-
-import time
+class Test: ##Defining the class
+    def m1 (self, a, b): ##defining a method
+        self.a = a
+        self.b = b
+        print(a + b)     
+t = Test() ##defining the object
+t.m1(23533,5850) ##method calling
 
 

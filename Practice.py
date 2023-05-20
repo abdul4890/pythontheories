f80d36ee4616a934a0448ce810f96e7500316927
abdul4890 <abdulraheman4890@gmail.com>
2023-05-20 20:40:46 +0530

    WAP to write PM

--- Practice.py
+++ Practice.py
@@ -1,9 +1,11 @@
-import pyautogui as pg
-import random
-import time
-while True:
-    x = random.randint(500,600)
-    y = random.randint(600,700)
-    pg.moveTo(x,y,0.5)
-    time.sleep(4)
+number = int(input('enter the number:'))
+if number<2:
+    print("Not prime")
+else:
+        for i in range(2,number):
+            if number%i== 0:
+             print("not prime")
+            break
+        else:
+            print("number is prime")
 

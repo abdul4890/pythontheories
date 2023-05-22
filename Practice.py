53d7faf13f5c8236451d33f522b7c5c7b1d7376b
abdul4890 <abdulraheman4890@gmail.com>
2023-05-23 00:07:59 +0530

    new

--- Practice.py
+++ Practice.py
@@ -1,9 +1,13 @@
-import random
-while True:
- print('1: roll the dice', '2: To exit')
-user = int(input('what do you want to do'))
-if user == 1:
- number= random.randit(1,6)
- print(number)
-else:
-    break
+# import random
+# while True:
+#  print('1: roll the dice', '2: To exit')
+# user = int(input('what do you want to do'))
+# if user == 1:
+#  number= random.randit(1,6)
+#  print(number)
+# else:
+#     break
+
+user = str(input("Enter a phrase: "))
+text = user.split()
+print(text)

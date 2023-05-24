c04dff07509d9c2994693676d8d5c8a5da24dc3e
abdul4890 <abdulraheman4890@gmail.com>
2023-05-25 00:45:57 +0530

    new

--- newproject.py
+++ newproject.py
@@ -1,10 +1,7 @@
 # WAP to get unique vovels from a string
 s = "WAP to get unique vovels from a string aaa A"
-
-for char in s:
-    if char.lower() in 'aeiou':
-            print(char)
-
+text = s.split()
+print(text)
 
 
    

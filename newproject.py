2fd7b7986cf4db1fec16c3afe35532b99ff5d420
abdul4890 <abdulraheman4890@gmail.com>
2023-05-26 20:20:25 +0530

    new

--- newproject.py
+++ newproject.py
@@ -1,10 +1,14 @@
-# WAP to get unique vovels from a string
-s = "WAP to get unique vovels from a string aaa A"
-text = s.split()
-a = ""
-for i in text:
-    a = a+i[0]
-    print(a)
+# # WAP to get unique vovels from a string
+# s = "WAP to get unique vovels from a string aaa A"
+# text = s.split()
+# a = ""
+# for i in text:
+#     a = a+i[0]
+#     print(a)
 
+
+a = []
+for i in range(0,11):
+    print(a)
 
    

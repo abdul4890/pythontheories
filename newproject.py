4a6fbbff649ae47d66efc911cba941473ea2ffee
abdul4890 <abdulraheman4890@gmail.com>
2023-06-02 17:53:30 +0530

    new

--- newproject.py
+++ newproject.py
@@ -3,16 +3,24 @@
 # text = s.split()
 # a = ""
 # for i in text:
-#     a = a+i[0]
-#     print(a)
+# #     a = a+i[0]
+# #     print(a)
 
 
-# a = []
-# for i in range(0,18):
-#     print(a)
+# # a = []
+# # for i in range(0,18):
+# #     print(a)
 
-a = [1, 2, 3, 4, 5]
-sum = 0 
-for ele in a: 
-  sum += ele 
-print(sum)
+# a = [1, 2, 3, 4, 5]
+# sum = 0 
+# for ele in a: 
+#   sum += ele 
+# print(sum)
+
+def MathChallenge(num):
+
+  # code goes here
+   return num
+
+# keep this function call here 
+print(MathChallenge(input()))

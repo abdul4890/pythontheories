abf1d3c96d5a4fb2044f411015d5d27137082f02
abdul4890 <abdulraheman4890@gmail.com>
2023-06-16 23:55:11 +0530

    new

--- newproject.py
+++ newproject.py
@@ -1,9 +1,4 @@
-# # WAP to get unique vovels from a string
-# s = "WAP to get unique vovels from a string aaa A"
-# text = s.split()
-# a = ""
-# for i in text:
-# #     a = 
 
-num = 23
-if 
+import random
+passlen = int(input("enter the password length"))
+s= "abcdefghijklmnopqrstuvwxyz1234567890"

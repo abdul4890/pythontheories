3cb2cd4a6a5ac7470ff866603a7f783cc4b66c22
abdul4890 <abdulraheman4890@gmail.com>
2023-06-28 23:22:22 +0530

    numebr of vowels

--- newproject.py
+++ newproject.py
@@ -1,3 +1,7 @@
-str = "any thing cn be done"
-a = len(str)
-print(a)
+string = input("Enter the input")
+vowels = 0
+for i in string:
+    if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'):
+     vowels = vowels + 1
+    print("the number of vowels are:")
+    print(vowels)

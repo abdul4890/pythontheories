1a139a5b1857696af533b70aa70f11b114c9da55
abdul4890 <abdulraheman4890@gmail.com>
2023-06-29 00:17:07 +0530

    new

--- newproject.py
+++ newproject.py
@@ -3,5 +3,7 @@ vowels = 0
 for i in string:
     if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'):
      vowels = vowels + 1
+     
     print("the number of vowels are:")
+    
     print(vowels)

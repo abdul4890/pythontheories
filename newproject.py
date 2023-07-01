d28e4fb8c791c0a2b5d80a9c6087f5b89a22ce28
abdul4890 <abdulraheman4890@gmail.com>
2023-07-01 16:02:16 +0530

    new

--- newproject.py
+++ newproject.py
@@ -1,7 +1,1 @@
-string = input("Enter the input")
-vowels = 0
-for i in string:
-    if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'):
-     vowels = vowels + 2
-    print("the number of vowels are:")
-    print(vowels)
+Def if 

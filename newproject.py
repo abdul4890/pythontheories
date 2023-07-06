ca7acd64773320dc7f2b22aadea5964c9a825cc6
abdul4890 <abdulraheman4890@gmail.com>
2023-07-06 23:31:03 +0530

    new

--- newproject.py
+++ newproject.py
@@ -1,11 +1,12 @@
 #python program to check maximum of 2 numbers
-
 def max(a, b):
-
+     
     if a >= b:
         return a
     else:
         return b
-    a = 44
-    b = 2
+     
+# Driver code
+a = 200
+b = 4
 print(max(a, b))

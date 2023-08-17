3a8897c053acf2e3ccf1fa013a36daf48b24962f
abdul4890 <abdulraheman4890@gmail.com>
2023-08-17 23:55:25 +0530

    new

--- Sum.py
+++ Sum.py
@@ -1,5 +1,5 @@
 #WAP to write multiplication of a number
-num = int(input("DispZlay the multiplication of: "))
+num = int(input("Display the multiplication of: "))
 
 for i in range(1,13):
     print(num, "x", i, "=", num*i)

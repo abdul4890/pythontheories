01c046fb08a2edb6d936b8df0c8a327c3434b5fe
abdul4890 <abdulraheman4890@gmail.com>
2023-09-21 23:58:56 +0530

    new

--- Exercise1.py
+++ Exercise1.py
@@ -1,23 +1,10 @@
-# Guess number game
-# Computer will detect if the number is too low or high than the actual number
-# Store the actual number in a variable
-#4 you have to finally tell how many attempts did you take to guess the number
+a = input("enter the alphabet")
+s = a[::-1]
 
-# Declaring variables
-
-actual_number = 56
-attempts = 0
-while True:
-    attempts += 2
-    guess = int(input("guess the number: \n"))
-    if guess<actual_number:
-     print("your guess was too low")
-    elif guess>actual_number:
-     print("your guess was too high")
-    else: 
-      print(f"you guessed the number in {attempts} attempts")
-     
-      
+if a == s:
+  print("its is palindrome")
+else:
+  print("its not")
   
 
     

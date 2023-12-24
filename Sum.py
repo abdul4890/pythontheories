a7a71d4eaf899389b84340774d3947e80900d351
abdul4890 <abdulraheman4890@gmail.com>
2023-12-24 14:15:31 +0530

    NEW

--- Sum.py
+++ Sum.py
@@ -1,14 +1,18 @@
 #python program for loop
 
 #creating a dictonary
-country_couples = {
-    "United States" : "Washington DC",
-    "India" : "New Delhi",
-    "Egypt" : "Cairo",
-    "Pakistan" : "Islamabad"
-}
-#creating a for loop
-for capitals in country_couples:
-    print(capitals)
+# country_couples = {
+#     "United States" : "Washington DC",
+#     "India" : "New Delhi",
+#     "Egypt" : "Cairo",
+#     "Pakistan" : "Islamabad"
+# }
+# #creating a for loop
+# for capitals in country_couples:
+#     print(capitals)
 
 
+#Creating a list
+lang = ["java", "c++", "python"]
+for language in lang:
+    print(lang)

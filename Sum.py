88346c85fd488c39b7140174cef3fd280e0485f0
abdul4890 <abdulraheman4890@gmail.com>
2023-12-31 15:04:34 +0530

    new

--- Sum.py
+++ Sum.py
@@ -43,14 +43,14 @@
 #  else:
 #     print(number, "its a prime number")
         
-#Python programme to check the largest of 3 numbers
-num1 = int(input("enter number 1:"))
-num2 = int(input("enter number 2:"))
-num3 = int(input("enter number 3:"))
+# #Python programme to check the largest of 3 numbers
+# num1 = int(input("enter number 1:"))
+# num2 = int(input("enter number 2:"))
+# num3 = int(input("enter number 3:"))
 
-if (num1 >= num2) and (num1 >= num3):
-    print("num 1 is largest")
-elif (num2 >= num1) and (num2 >= num3):
-        print("num 2 is largest")
-else:
-            print("number 3 is largest")
+# if (num1 >= num2) and (num1 >= num3):
+#     print("num 1 is largest")
+# elif (num2 >= num1) and (num2 >= num3):
+#         print("num 2 is largest")
+# else:
+#             print("number 3 is largest")
